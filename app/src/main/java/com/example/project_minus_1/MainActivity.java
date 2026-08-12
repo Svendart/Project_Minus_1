5e9fc69a0a07a092e0d759511dc5ecd8e18b117c
Ahsan Drajad Mulia <ahsandrajadmulia1@gmail.com>
2026-08-12 14:55:27 +0700

    intial commit

--- app/src/main/java/com/example/project_minus_1/MainActivity.java
+++ app/src/main/java/com/example/project_minus_1/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    
 
     // Tambahkan variabel textmu di textview ke java dulu
     TextView text;
@@ -23,12 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Tambahkan perintah untuk mencari id text di java
+        text = findViewById(R.id.text);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-            // Tambahkan perintah untuk mencari id text di java
-            text = findViewById(R.id.text);
 
             return insets;
         });
@@ -44,8 +46,18 @@ public class MainActivity extends AppCompatActivity {
     // Fungsi ini dipanggil saat tombol HITUNG diklik
     public void countUp(View view) {
         count++; // Menambah angka
-        text.setText(count + ""); // Menampilkan ke layar
+        text.setText(String.valueOf(count)); // Menampilkan ke layar
+
+        if (count % 10 == 0) {
+            text.setBackgroundColor(getColor(R.color.biru));
+        } else {
+            text.setBackgroundColor(getColor(R.color.hijau));
+        }
     }
 
-
+    public void showReset(View view) {
+        count = 0; // Mereset angka
+        text.setText("0"); // Menampilkan ke layar
+        text.setBackgroundColor(getColor(R.color.biruTua)); // Mereset warna ke awal
+    }
 }
